ffd25739c65fe68e9c31ee08ecb880634f11f78b
Your Name <you@example.com>
2024-08-19 11:19:26 -0500

    LH.Games

--- projeto-angular/projeto-angular/loja-games/README.md
+++ projeto-angular/projeto-angular/loja-games/README.md
@@ -1,4 +1,4 @@
-# ProjetinhoAngular
+# LojaGames
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.4.
 
--- projeto-angular/projeto-angular/loja-games/src/app/app.module.ts
+++ projeto-angular/projeto-angular/loja-games/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
 /*Importações Angular Material*/
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
@@ -12,8 +11,6 @@ import {MatInputModule} from '@angular/material/input';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -21,7 +18,6 @@ import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
-
 
 @NgModule({
   declarations: [
